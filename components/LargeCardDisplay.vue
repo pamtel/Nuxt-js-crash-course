e4f932ece44fd3394858be938e7a4def917f2fee
pamtel <pamelaakosahiante@gmail.com>
2021-09-13 15:42:55 +0100

    first commit

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <h4 class="header">Hello World</h4>
+    <p class="snippet">Lorem ipsum dolor sit</p>
+    <div class="card-container"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+  .container {
+    margin-top: 2rem;
+}
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+    .snippet {
+        color: grey;
+        margin-bottom: 1.5rem;
+    }
+    .cards-container {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Hero />
+  <LargeCardDisplay />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
